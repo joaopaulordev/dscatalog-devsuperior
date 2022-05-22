ca80fc1df07ed9046eb4b293d7c8120b27dad2b1
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2022-05-22 14:55:14 -0300

    auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO TB_CATEGORY(NAME) VALUES('Books');
-INSERT INTO TB_CATEGORY(NAME) VALUES('Electronics');
-INSERT INTO TB_CATEGORY(NAME) VALUES('Computers');
+INSERT INTO TB_CATEGORY(NAME, created_at) VALUES('Books', now());
+INSERT INTO TB_CATEGORY(NAME, created_at) VALUES('Electronics', now());
+INSERT INTO TB_CATEGORY(NAME, created_at) VALUES('Computers', now());
